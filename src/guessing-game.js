5a6441b4b5347ea2ab234fe6d15ef7e046d139a9
panamarova <ludpnot@gmail.com>
2022-08-16 13:41:06 +0300

    fix: no

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -6,17 +6,6 @@ class GuessingGame {
         this.max = max;
     }
 
-    guess() {
-        return Math.round((this.min + this.max) / 2);
-    }
 
-    lower() {
-        return this.max = Math.round((this.min + this.max) / 2);
-    }
-
-    greater() {
-        return this.min = Math.round((this.min + this.max) / 2);
-    }
-}
 
 module.exports = GuessingGame;
